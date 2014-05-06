bfd3976390a53cc3ab7e6c4afaab394bca6cf444
robinherleman <robin.herleman@gmail.com>
2014-05-06 20:22:55 +0200

    Introducing : Users

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BasicAuth::Application.config.secret_token = '9c26da72b8b2ea23ed1a6fa35485f87046599c1ebe5a9df1347c30f0a504f98f4d54e868b5e7552ebfefb60c2a14cbbee8818dc4e42316ad90ee424bf54420b1'
+RailsBB::Application.config.secret_token = '9c26da72b8b2ea23ed1a6fa35485f87046599c1ebe5a9df1347c30f0a504f98f4d54e868b5e7552ebfefb60c2a14cbbee8818dc4e42316ad90ee424bf54420b1'
